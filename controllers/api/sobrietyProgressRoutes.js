2f051ddea9cdfdd0e69d3780bb66f02757506842
BenThere <benjaminbirdsall@icloud.com>
2023-10-11 17:08:33 -0600

    add post route

--- controllers/api/sobrietyProgressRoutes.js
+++ controllers/api/sobrietyProgressRoutes.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
 const { SobrietyProgress } = require('../../models');
 
+router.post('/', async (req, res) => {
+    try {
+        const newProgress = await SobrietyProgress.create(req.body);
+        res.status(201).json(newProgress);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
 
 module.exports = router;
