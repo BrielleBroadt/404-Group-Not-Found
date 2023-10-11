bab3e97b8c4630604989aaeaaeaafa9e02811511
Anthony Littlejohn <ant05man@yahoo.com>
2023-10-10 20:37:35 -0400

    added routes to controllers' index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const entriesRoutes = require('./entriesRoutes');
+const sobrietyProgressRoutes = require('./sobrietyProgressRoutes');
 
 router.use('/users', userRoutes);
+router.use('entries', entriesRoutes);
+router.use('/progress', sobrietyProgressRoutes);
 
 module.exports = router;
