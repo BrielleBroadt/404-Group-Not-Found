83c39770d48357654de919dc684c4a47a1427772
BenThere <benjaminbirdsall@icloud.com>
2023-10-11 17:11:59 -0600

    added put and delete routes

--- controllers/api/entriesRoutes.js
+++ controllers/api/entriesRoutes.js
@@ -25,6 +25,34 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedEntry = await Entries.update(req.body, {
+            where: { id: req.params.id },
+        });
+        if (updatedEntry[0] === 0) {
+            res.status(404).json({ message: 'Entry not found' });
+            return;
+        }
+        res.status(200).json({ message: 'Entry updated successfully' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedCount = await Entries.destroy({
+            where: { id: req.params.id },
+        });
+        if (deletedCount === 0) {
+            res.status(404).json({ message: 'Entry not found' });
+            return;
+        }
+        res.status(204).end(); // No content, successful deletion
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
