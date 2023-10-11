d5096e66001843c7d425a6f3bd51443f6b4f1627
Brielle <brielleb.broadt@gmail.com>
2023-10-11 18:41:11 -0500

    fixing issues with node server.js

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ var data = {
   imageAltText: "One Day At a Time"
 };
 // Testing adding photo to home page
-var source = document.getElementById("template").innerHTML;
-var template = Handlebars.compile(source);
-var html = template(data);
-document.getElementById("output").innerHTML = html;
+// var source = document.getElementById("template").innerHTML;
+// var template = Handlebars.compile(source);
+// var html = template(data);
+// document.getElementById("output").innerHTML = html;
 
 app.use(session(sess));
 
