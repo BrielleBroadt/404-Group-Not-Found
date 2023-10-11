d58f6db6547e920b0c231fb0ee59d7998449a5f4
BenThere <benjaminbirdsall@icloud.com>
2023-10-11 17:08:26 -0600

    add post and get route

--- controllers/api/entriesRoutes.js
+++ controllers/api/entriesRoutes.js
@@ -1,5 +1,30 @@
 const router = require('express').Router();
 const { Entries } = require('../../models');
 
+router.post('/', async (req, res) => {
+    try {
+        const { per_day, cost_each, user_id } = req.body;
+
+        const newEntry = await Entries.create({
+            per_day,
+            cost_each,
+            user_id,
+        });
+        res.status(200).json(newEntry);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const entries = await Entries.findAll();
+        res.status(200).json(entries);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
 
 module.exports = router;
