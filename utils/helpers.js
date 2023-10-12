84083d76a4f3ac6b324e251d4834c21a46984a6b
Brielle <brielleb.broadt@gmail.com>
2023-10-11 19:24:51 -0500

    coding for emojis

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,20 +3,20 @@ module.exports = {
     // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
   },
-  format_amount: (amount) => {
+  format_timesober: (timesober) => {
     // format large numbers with commas
-    return parseInt(amount).toLocaleString();
+    return parseInt(timesober).toLocaleString();
   },
   get_emoji: () => {
     const randomNum = Math.random();
 
     // Return a random emoji
     if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
+      return `<span for="img" aria-label="peach">🍑</span>`;
     } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
+      return `<span for="img" aria-label="avacado">🥑</span>`;
     } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
+      return `<span for="img" aria-label="strawberry">🍓</span>`;
     }
   },
 };
