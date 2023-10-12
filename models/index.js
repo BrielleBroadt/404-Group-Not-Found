2b65d2bb6468ff6259c52f7329a1cbb3d7755e64
Anthony Littlejohn <ant05man@yahoo.com>
2023-10-12 09:58:32 -0400

    added hasmany function to models' index

--- models/index.js
+++ models/index.js
@@ -1,4 +1,11 @@
 const User = require('./User');
+const Entries = require('./entries');
+const SobrietyProgress = require('./sobrietyProgress')
+
+User.hasMany(Entries, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
 // User.hasMany(Project, {
 //   foreignKey: 'user_id',
@@ -9,4 +16,5 @@ const User = require('./User');
 //   foreignKey: 'user_id'
 // });
 
-module.exports = { User };
+module.exports = { User, Entries, SobrietyProgress };
+
