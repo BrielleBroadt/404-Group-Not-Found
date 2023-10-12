7d8c628fdb07ce975ac76c2e1ccb16d997420c29
Anthony Littlejohn <ant05man@yahoo.com>
2023-10-12 10:39:10 -0400

    added password hash

--- models/User.js
+++ models/User.js
@@ -31,9 +31,20 @@ User.init(
     password: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: [8],
+      }
     },
   },
+  // this hashes the password
   {
+    hooks: {
+        beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData
+        }
+    },
+
     sequelize,
     timestamps: false,
     freezeTableName: true,
