82390cf1d54fdfe43c592c34235170fa5084c3e5
Brielle <brielleb.broadt@gmail.com>
2023-10-11 20:04:13 -0500

    completd helper.js file

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,8 @@
 module.exports = {
-  format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
-  },
-  format_timesober: (timesober) => {
+  
+  format_data: (data) => {
     // format large numbers with commas
-    return parseInt(timesober).toLocaleString();
+    return parseInt(data).toLocaleString();
   },
   get_emoji: () => {
     const randomNum = Math.random();
