4ec8dd1664a88ede027c94ddd4c1761952619510
Anthony Littlejohn <ant05man@yahoo.com>
2023-10-12 10:12:38 -0400

    finished relations inside models index.js

--- models/index.js
+++ models/index.js
@@ -6,7 +6,12 @@ User.hasMany(Entries, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
-
+Entries.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+SobrietyProgress.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 // User.hasMany(Project, {
 //   foreignKey: 'user_id',
 //   onDelete: 'CASCADE'
